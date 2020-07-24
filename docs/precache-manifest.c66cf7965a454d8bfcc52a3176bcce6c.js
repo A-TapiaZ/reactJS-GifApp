adef2203881ede72c4a0eec19fb365ac70d2c4cb
Alejo Tapias <alejotapias0414@gmail.com>
2020-07-23 19:34:23 -0500

    docs(build) agregado

---
+++ docs/precache-manifest.c66cf7965a454d8bfcc52a3176bcce6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b26dd05cc660abd030d8e185e518c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08468280a2e033475447",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "f015b618dee8088e2525",
+    "url": "/static/css/main.cae80e76.chunk.css"
+  },
+  {
+    "revision": "08468280a2e033475447",
+    "url": "/static/js/2.4c07c628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c07c628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f015b618dee8088e2525",
+    "url": "/static/js/main.a50cca89.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
